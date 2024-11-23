9be2b311246b3b05bd50bc3b366fb55922f9d051
Lucas Tanaka <160812489+Chico7854@users.noreply.github.com>
2024-11-23 15:53:30 -0300

    Make functions to update the display
    Create functions for when buttons that are not operators are clicked
    Functions to erase display when hit "C", to change the sign and
    to update the display with numbers

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//basic math functions
 function add(a, b) {
     return a + b;
 }
@@ -27,6 +28,7 @@ function operate(a, b, operation) {
     }
 }
 
+//dinamically calculate the width of elements
 function defineWidthOfDoubleButtons() {
     let buttonWidth = parseInt(getComputedStyle(numberButton).width);
     let gap = parseInt(getComputedStyle(buttonsContainer).gap);
@@ -45,10 +47,58 @@ function defineWidthOfCalculator() {
     buttonsContainer.style.width = `${size}px`;
 }
 
-
 const doubleButtons = Array.from(document.querySelectorAll(".doubleButton"));
 const numberButton = document.querySelector(".numberButton");
 const buttonsContainer = document.querySelector("#buttons");
 
 defineWidthOfDoubleButtons();
 defineWidthOfCalculator();
+
+function displayNumber(button) {
+    display.textContent = display.textContent + button.textContent;
+}
+
+function clearDisplay() {
+    display.textContent = "";
+}
+
+function getFirstClass(classes) {
+    let classesArray = classes.split(" ");
+    return classesArray.length === 1 ? classes : classesArray[0];
+}
+
+function changeNumberSign() {
+    display.textContent[0] === "-"
+        ? display.textContent = display.textContent.slice(1)
+        : display.textContent = "-" + display.textContent
+}
+
+function updateDisplay(button) {
+    let buttonClass = getFirstClass(button.className);
+    let textContent = button.textContent;
+
+    if (textContent === "C") {
+        clearDisplay();
+        return;
+    } else if (textContent === "+/-") {
+        
+    }
+
+
+    switch (className) {
+        case "optionButton" {
+            
+        }
+    }
+}
+
+const display = document.querySelector("#display p");
+let firtsNumber;
+
+buttonsContainer.addEventListener("click", event => {
+    let target = event.target;
+    if (target.tagName === "DIV") stopImediatePropagation();
+    updateDisplay(target);
+})
+
+
