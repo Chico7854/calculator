8c4bc433f60e2829508a8757acb5b2e93f4b2075
Lucas Tanaka <160812489+Chico7854@users.noreply.github.com>
2024-11-23 03:14:21 -0300

    Create function to select operation

--- javascript.js
+++ javascript.js
@@ -13,3 +13,16 @@ function multiply(a, b) {
 function divide(a, b) {
     return b === 0 ? "dumbass" : a / b;
 }
+
+function operate(a, b, operation) {
+    switch (operation) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
+}
